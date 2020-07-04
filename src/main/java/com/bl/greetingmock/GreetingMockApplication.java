4fcde224a3135c1ebebda634177a7c8a839e9be8
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-07-04 21:27:28 +0530

    Initial Commit

---
+++ src/main/java/com/bl/greetingmock/GreetingMockApplication.java
@@ -0,0 +1,13 @@
+package com.bl.greetingmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingMockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bl/greetingmock/GreetingMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bl.greetingmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingMockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
